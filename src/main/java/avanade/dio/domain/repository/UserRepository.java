4475492d5e001244929da1fb40aec66bab31358e
Daniel <danielwick12@hotmail.com>
2025-03-18 00:01:16 -0300

    create user repository

---
+++ src/main/java/avanade/dio/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package avanade.dio.domain.repository;
+
+import avanade.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByMember(String accountNumber);
+}
