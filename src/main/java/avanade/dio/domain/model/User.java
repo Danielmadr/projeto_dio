84cbc04c79893321c0f74e1a1835624bf85b833e
Daniel <danielwick12@hotmail.com>
2025-03-18 00:00:59 -0300

    make user email unique

--- src/main/java/avanade/dio/domain/model/User.java
+++ src/main/java/avanade/dio/domain/model/User.java
@@ -12,6 +12,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
